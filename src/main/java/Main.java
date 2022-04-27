d373917bddbd836e840db641eac8d3dc5712ff2c
santi-g-s <santiago@garciasantos.org>
2022-04-27 19:03:15 -0400

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import masecla.reddit4j.client.Reddit4J;
+
+import masecla.reddit4j.client.ClientType;
+
+import masecla.reddit4j.objects.RedditUser;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        private RedditClient mockRedditClient;
+    }
+
+}
+
