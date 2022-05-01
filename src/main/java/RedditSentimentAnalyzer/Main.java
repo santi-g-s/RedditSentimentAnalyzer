28ee6608a4571331b7ee848b197ddcb70652fa64
santi-g-s <santiago@garciasantos.org>
2022-04-30 22:17:41 -0400

    Parses through subreddit Posts in a given time window.

--- src/main/java/RedditSentimentAnalyzer/Main.java
+++ src/main/java/RedditSentimentAnalyzer/Main.java
@@ -1,5 +1,8 @@
 package RedditSentimentAnalyzer;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,7 +15,19 @@ public class Main {
         List<String> tickers = new LinkedList<>();
         tickers.add("RBLX");
 
-        parser.getPostsForSubreddit("stocks", tickers);
+        SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            Date d1 = sdformat.parse("2022-01-01");
+            Date d2 = sdformat.parse("2022-01-31");
+
+            parser.getPostsForSubreddit("stocks", tickers,d1, d2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 
